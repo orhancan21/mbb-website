56c3e660840f2825511372ced339f8530d8424d4
Eleven <incealper00@gmail.com>
2021-03-31 10:02:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var app = require('express').createServer();
-
-app.get('/', function(req, res){
-  res.render('/pages/index.html');
-});
-
-app.get('/*', function(req, res){
-  res.render('/pages/404.html');
-});
-
-app.listen(1243);
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.json());
+app.use(express.static("express"));
+app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/pages/index.html'));
+  });
+app.use('/*', function(req,res){
+    res.sendFile(path.join(__dirname+'/pages/404.html'));
+  });
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
