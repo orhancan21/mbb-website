111d97786019c86387e642aea793e93375423c6d
Eleven <incealper00@gmail.com>
2021-04-03 13:59:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const express = require("express");
 var app = require('express')//.createServer();
+const path = require("path");
+
+app.use(express.static("public"));
+  app.use(
+    "/css",
+    express.static(path.resolve(__dirname + `/css`))
+  );
+
+  app.use(
+    "/js",
+    express.static(path.resolve(__dirname + `/js`))
+  );
 
 app.get('/', function(req, res){
-  res.render('pages/index.html');
+  res.render('/pages/index.html');
 });
 
 app.get('/*', function(req, res){
-  res.render('pages/404.html');
+  res.render('/pages/404.html');
 });
 
 app.listen(1243);
