72ff021c3aba47c27fbac5a386a563748dbb4774
Eleven <incealper00@gmail.com>
2021-04-03 14:03:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const express = require("express");
 var app = require('express')//.createServer();
-const path = require("path");
-
-app.use(express.static("public"));
-  app.use(
-    "/css",
-    express.static(path.resolve(__dirname + `/css`))
-  );
-
-  app.use(
-    "/js",
-    express.static(path.resolve(__dirname + `/js`))
-  );
 
 app.get('/', function(req, res){
   res.render('/pages/index.html');
